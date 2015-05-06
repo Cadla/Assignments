5f2d85f1207863439a5fb52e61406a000ae63641
ravitalveda <ravitalveda@gmail.com>
2015-05-06 12:35:31 +0800

    Commiting from Office system

--- RandomNumbers/Program.cs
+++ RandomNumbers/Program.cs
@@ -6,34 +6,102 @@ using System.Threading.Tasks;
 
 namespace RandomNumbers
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Decide a number to let the computer guess it: ");
+            Console.WriteLine("Decide a number from 1 to 100 to let the computer guess it: ");
             int enteredNumber = Int32.Parse(Console.ReadLine());
 
             Random r = new Random();
 
-            //Gets next random number ranging from 1 to 10.
-            int randomNumber = r.Next(1, 100);
+            //Gets next random number ranging from 1 to 100.
+            int min = 1;
+            int max = 100;
+            int computerGussedNumber = r.Next(min, max);
+            int counter = 0;
+            int maxTrials = 7;
 
-            while (enteredNumber != randomNumber)
+            while (enteredNumber != computerGussedNumber && counter <= maxTrials)
             {
+                if (computerGussedNumber > enteredNumber)
+                {
+                    Console.WriteLine(string.Format("Computer guess {0} is higher  ", computerGussedNumber));
+                    //Set max range by narrowing the search area. Set max
+                    if (min < computerGussedNumber - 15 && enteredNumber < computerGussedNumber - 15)
+                    {
+                        max = computerGussedNumber - 15;
+                        Console.WriteLine("Since {0} is too higher than user input, it's max range is reduced by 15", computerGussedNumber);
+                    }
+                    else if (min < computerGussedNumber - 10 && enteredNumber < computerGussedNumber - 10)
+                    {
+                        max = computerGussedNumber - 10;
+                        Console.WriteLine("Since {0} is a bit higher it's max range is reduced by 10", computerGussedNumber);
+                    }
+                    else if (min < computerGussedNumber - 5 && enteredNumber < computerGussedNumber - 5)
+                    {
+                        max = computerGussedNumber - 5;
+                        Console.WriteLine("Since {0} is little higher it's max range is reduced by 5", computerGussedNumber);
+                    }
+                    else
+                    {
+                        max = computerGussedNumber - 1;
+                        Console.WriteLine("Since {0} is little higher it's max range is reduced by 1", computerGussedNumber);
+                    }
+                    
+                    //Minimize search area. Take a number from min to current guess
+                    computerGussedNumber = r.Next(min, max);
 
-                if (randomNumber > enteredNumber)
-                    Console.WriteLine(string.Format("Computer guess {0} is higher  ", randomNumber));
+                }
+                else if (computerGussedNumber < enteredNumber)
+                {
+                    Console.WriteLine(string.Format("Computer guess {0} is lower", computerGussedNumber));
+                    // set min range from current system guess. Since it's lower let the system search from next number to current low.
+                    if (max > computerGussedNumber + 15 && enteredNumber > computerGussedNumber + 15)
+                    {
+                        min = computerGussedNumber + 15;
+                        Console.WriteLine("Since {0} is too lower than user input, it's min range is increased by 15", computerGussedNumber);
+                    }
+                    else if (max > computerGussedNumber + 10 && enteredNumber > computerGussedNumber + 10)
+                    {
+                        min = computerGussedNumber + 10;
+                        Console.WriteLine("Since {0} is lower it's min range is increased by 10", computerGussedNumber);
+                    }
+                    else if (max > computerGussedNumber + 5 && enteredNumber > computerGussedNumber + 5)
+                    {
+                        min = computerGussedNumber + 5;
+                        Console.WriteLine("Since {0} is a bit lower it's min range is increased by 5", computerGussedNumber);
+                    }
+                    else
+                    {
+                        min = computerGussedNumber + 1;
+                        Console.WriteLine("Since {0} is a little lower it's min range is increased by 1", computerGussedNumber);
+                    }
+                    computerGussedNumber = r.Next(min, max);
+                }
 
-                else if (randomNumber < enteredNumber)
-                    Console.WriteLine(string.Format("Computer guess {0} is lower", randomNumber));
 
-                randomNumber = r.Next(1, 100);
+                Console.WriteLine("Computer Random Search Area: \nMinvalue: " + min.ToString());
+                Console.WriteLine("Maxvalue: " + max.ToString());
                 Console.ReadLine();
+                counter++;
+
+                if (counter == maxTrials)
+                {
+                    Console.WriteLine("Sorry computer. You've used maximum trials. Better luck next time!");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Computer has {0} more trials \n", maxTrials - counter);
+                }
+
             }
 
             //Here the number matches & writes the result to console.
-            Console.WriteLine(string.Format("Super! This time computer has made it. The user input is: {0}", randomNumber));
+            Console.WriteLine(string.Format("Super! This time computer has made it. The user input is: {0}", computerGussedNumber));
             Console.ReadLine();
         }
 
