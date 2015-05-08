aa2b4cd17d73adf25734bb9607360b54a5f2e072
ravitalveda <ravitalveda@gmail.com>
2015-05-08 22:51:47 +0800

    Without a list and Median, but still Janusz won

--- RandomNumbers/Program.cs
+++ RandomNumbers/Program.cs
@@ -10,32 +10,37 @@ namespace RandomNumbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Decide a number from 1 to 100 to let the computer guess it: ");
+            int userInput = Int32.Parse(Console.ReadLine());
+            int computerGuess = 50, counter = 0, min=1, max=100;
+            const int maxTrials = 7;
 
-            Console.WriteLine("Decide a number to let the computer guess it: ");
-            int enteredNumber = Int32.Parse(Console.ReadLine());
-
-            Random r = new Random();
-
-            //Gets next random number ranging from 1 to 10.
-            int randomNumber = r.Next(1, 100);
-
-            while (enteredNumber != randomNumber)
+            while (userInput != computerGuess && counter <= maxTrials)
             {
+                if (computerGuess > userInput)
+                {
+                    Console.WriteLine(string.Format("Computer guess {0} is higher  ", computerGuess));
+                    max = computerGuess;
+                    computerGuess = min + (max - min) / 2;
+                }
+                else if (computerGuess < userInput)
+                {
+                    Console.WriteLine(string.Format("Computer guess {0} is lower", computerGuess));
+                    min = computerGuess;
+                    computerGuess = max - (max - min) / 2;
+                }
 
-                if (randomNumber > enteredNumber)
-                    Console.WriteLine(string.Format("Computer guess {0} is higher  ", randomNumber));
-
-                else if (randomNumber < enteredNumber)
-                    Console.WriteLine(string.Format("Computer guess {0} is lower", randomNumber));
-
-                randomNumber = r.Next(1, 100);
+                Console.WriteLine("Min Range: " + min);
+                Console.WriteLine("Max Range: " + max);
                 Console.ReadLine();
+                counter++;
+
+                Console.WriteLine("Computer has {0} more trials \n", maxTrials - counter);
             }
 
             //Here the number matches & writes the result to console.
-            Console.WriteLine(string.Format("Super! This time computer has made it. The user input is: {0}", randomNumber));
+            Console.WriteLine(string.Format("Super! This time computer has made it. The user input is: {0}", computerGuess));
             Console.ReadLine();
         }
-
     }
 }
