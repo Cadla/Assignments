4febbae21a4e1740d8756a4b296651d6385c98ba
ravitalveda <ravitalveda@gmail.com>
2015-05-13 07:47:47 +0800

    TestCasesAdded

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomNumbers;
+
+namespace UnitTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        Program p = new Program();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //First  
+            Program.TrianlgeSidesInput(10, 10, 15);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Second Test  
+            Trigonometric.TrianlgeSidesInput(10.5, 10.5, 15);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Thisrd Test  
+            Trigonometric.TrianlgeSidesInput(10.5, 12.5, 15.5);
+
+        }
+    }
+}
