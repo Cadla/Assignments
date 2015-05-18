9dfb7539de01e23ca1f1b1abc2c03780047299e1
ravitalveda <ravitalveda@gmail.com>
2015-05-18 10:09:00 +0800

    New Artcitecture With Test Classes

---
+++ Assignments.Test/NumberCollectionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assignments.Test
+{
+    [TestClass]
+    public class NumberCollectionTest
+    {
+        NumberCollection nc = new NumberCollection();
+
+        [TestMethod]
+        public void NumberCollection1()
+        {
+            bool isExsisting = nc.IsNumberInCollection(Enumerable.Range(1, 100), 75);
+            Assert.IsTrue(isExsisting);
+        }
+
+        [TestMethod]
+        public void NumberCollection2()
+        {
+            bool isExsisting = nc.IsNumberInCollection(Enumerable.Range(1, 100), 750);
+            Assert.IsTrue(isExsisting);
+        }
+
+        [TestMethod]
+        public void NumberCollection3()
+        {
+            bool isExsisting = nc.IsNumberInCollection(Enumerable.Range(1, 100), 1);
+            Assert.IsTrue(isExsisting);
+        }
+
+        [TestMethod]
+        public void NumberCollection4()
+        {
+            bool isExsisting = nc.IsNumberInCollection(Enumerable.Range(1, 100), 100);
+            Assert.IsTrue(isExsisting);
+        }
+
+        [TestMethod]
+        public void NumberCollection5()
+        {
+            //IEnumerable<int> squares = Enumerable.Range(4, 3)
+
+            bool isExsisting = nc.IsNumberInCollection(new int[] { 3, 4, 5, 6, 7, 9, 8, 10, 13, 15, 17 }, 8);
+            Assert.IsTrue(isExsisting);
+        }
+    }
+}
--- Assignments/RandomNumbers.cs
+++ Assignments/RandomNumbers.cs
@@ -4,24 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RandomNumbers
+namespace Assignments
 {
-    public class Program
+    public class RandomNumbers
     {
+        private static bool UserInputValidation(int userInput, int minRange, int maxRange)
+        {
+            return (userInput < minRange || userInput > maxRange) ? true : false;
+        }
 
-
-
-        public static int CountTrails(int userInput, int minRange, int maxRange)
+        public int CountTrails(int userInput, int minRange, int maxRange)
         {
             bool isNotInRange = UserInputValidation(userInput, minRange, maxRange);
 
             if (isNotInRange)
             {
                 Console.WriteLine("User input is not in range.");
-                Console.ReadLine();
                 Environment.Exit(0);
             }
-
 
             int counter = 1;
             double computerGuess = Math.Round(((double)maxRange - (double)minRange) / 2);
@@ -45,41 +45,9 @@ namespace RandomNumbers
 
                 Console.WriteLine("Min Range: " + minRange);
                 Console.WriteLine("Max Range: " + maxRange);
-                //Console.ReadLine();
                 counter++;
             }
             return counter;
-        }
-
-        private static bool UserInputValidation(int userInput, int minRange, int maxRange)
-        {
-            return (userInput < minRange || userInput > maxRange) ? true : false;                
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Collection of Numbers Program: \n");
-            IsNumberInCollection(new int[] { 3, 4, 5, 6 }, 8);
-
-
-            Console.WriteLine("Random Numbers Program: \n");
-            int count = CountTrails(50, 101, 100);
-
-            //Here the number matches & writes the result to console.
-            Console.WriteLine(string.Format("Super! Computer has made it in {0} trials!", count));
-            Console.ReadLine();
-            
-        }
-
-        private static void IsNumberInCollection(IEnumerable<int> collection, int number)
-        {
-            if (collection.Contains(number))
-                Console.WriteLine("{0} is in collection \n", number);
-            else
-                Console.WriteLine("{0} is not in collection \n", number);
-
-            Console.WriteLine("Please press enter to move to Random Numbers program!");
-            Console.ReadLine();
         }
     }
 }
